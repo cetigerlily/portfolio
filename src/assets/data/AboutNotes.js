d6766b3ccd29ba8e67fe10609cd3563dcbc35e4d
cetigerlily <celestigerlily@gmail.com>
2024-04-17 16:46:08 +0800

    Add in data for About and Projects pages

---
+++ src/assets/data/AboutNotes.js
@@ -0,0 +1,5 @@
+export const aboutNotes = [
+  "Year 3 Computer Science major at NUS focusing on SWE and AI/ML",
+  "Passionate about tech for good and empowering women in tech",
+  "Let's chat in one of the 5 languages I know!",
+]
---
+++ src/assets/data/Projects.js
@@ -0,0 +1,53 @@
+export const Projects = [
+  // TODO: move to a DB
+  {
+    name: "Wingman",
+    // TODO: add date formatter for the project dates
+    date: "MM YYYY - MM YYYY",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam aliquam urna sagittis iaculis elementum. Curabitur volutpat sed tellus ac dapibus. Donec ligula orci, dictum quis maximus vitae, faucibus at felis. Proin vel eros orci. Mauris eleifend orci at orci rutrum aliquam. In feugiat vestibulum arcu, quis convallis elit tincidunt eu. Praesent rhoncus varius sem, egestas luctus libero feugiat ac. Nam eget dui et eros tincidunt bibendum. Nullam dignissim, elit ac gravida condimentum, libero lacus condimentum nisi, quis cursus nunc dolor sit amet massa.",
+    // TODO: add in tags for projects i.e. based on language, task
+  },
+  {
+    name: "Tigerlily",
+    date: "MM YYYY - MM YYYY",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam aliquam urna sagittis iaculis elementum. Curabitur volutpat sed tellus ac dapibus. Donec ligula orci, dictum quis maximus vitae, faucibus at felis. Proin vel eros orci. Mauris eleifend orci at orci rutrum aliquam. In feugiat vestibulum arcu, quis convallis elit tincidunt eu. Praesent rhoncus varius sem, egestas luctus libero feugiat ac. Nam eget dui et eros tincidunt bibendum. Nullam dignissim, elit ac gravida condimentum, libero lacus condimentum nisi, quis cursus nunc dolor sit amet massa.",
+  },
+  {
+    name: "STEER",
+    date: "MM YYYY - MM YYYY",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam aliquam urna sagittis iaculis elementum. Curabitur volutpat sed tellus ac dapibus. Donec ligula orci, dictum quis maximus vitae, faucibus at felis. Proin vel eros orci. Mauris eleifend orci at orci rutrum aliquam. In feugiat vestibulum arcu, quis convallis elit tincidunt eu. Praesent rhoncus varius sem, egestas luctus libero feugiat ac. Nam eget dui et eros tincidunt bibendum. Nullam dignissim, elit ac gravida condimentum, libero lacus condimentum nisi, quis cursus nunc dolor sit amet massa.",
+  },
+  {
+    name: "PeerPrep",
+    date: "MM YYYY - MM YYYY",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam aliquam urna sagittis iaculis elementum. Curabitur volutpat sed tellus ac dapibus. Donec ligula orci, dictum quis maximus vitae, faucibus at felis. Proin vel eros orci. Mauris eleifend orci at orci rutrum aliquam. In feugiat vestibulum arcu, quis convallis elit tincidunt eu. Praesent rhoncus varius sem, egestas luctus libero feugiat ac. Nam eget dui et eros tincidunt bibendum. Nullam dignissim, elit ac gravida condimentum, libero lacus condimentum nisi, quis cursus nunc dolor sit amet massa.",
+  },
+  {
+    name: "Wingman",
+    date: "MM YYYY - MM YYYY",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam aliquam urna sagittis iaculis elementum. Curabitur volutpat sed tellus ac dapibus. Donec ligula orci, dictum quis maximus vitae, faucibus at felis. Proin vel eros orci. Mauris eleifend orci at orci rutrum aliquam. In feugiat vestibulum arcu, quis convallis elit tincidunt eu. Praesent rhoncus varius sem, egestas luctus libero feugiat ac. Nam eget dui et eros tincidunt bibendum. Nullam dignissim, elit ac gravida condimentum, libero lacus condimentum nisi, quis cursus nunc dolor sit amet massa.",
+  },
+  {
+    name: "Womengineer",
+    date: "MM YYYY - MM YYYY",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam aliquam urna sagittis iaculis elementum. Curabitur volutpat sed tellus ac dapibus. Donec ligula orci, dictum quis maximus vitae, faucibus at felis. Proin vel eros orci. Mauris eleifend orci at orci rutrum aliquam. In feugiat vestibulum arcu, quis convallis elit tincidunt eu. Praesent rhoncus varius sem, egestas luctus libero feugiat ac. Nam eget dui et eros tincidunt bibendum. Nullam dignissim, elit ac gravida condimentum, libero lacus condimentum nisi, quis cursus nunc dolor sit amet massa.",
+  },
+  {
+    name: "NUS Aerobics Webs",
+    date: "MM YYYY - MM YYYY",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam aliquam urna sagittis iaculis elementum. Curabitur volutpat sed tellus ac dapibus. Donec ligula orci, dictum quis maximus vitae, faucibus at felis. Proin vel eros orci. Mauris eleifend orci at orci rutrum aliquam. In feugiat vestibulum arcu, quis convallis elit tincidunt eu. Praesent rhoncus varius sem, egestas luctus libero feugiat ac. Nam eget dui et eros tincidunt bibendum. Nullam dignissim, elit ac gravida condimentum, libero lacus condimentum nisi, quis cursus nunc dolor sit amet massa.",
+  },
+  {
+    name: "Women in Tech @ NUS Computing",
+    date: "MM YYYY - MM YYYY",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam aliquam urna sagittis iaculis elementum. Curabitur volutpat sed tellus ac dapibus. Donec ligula orci, dictum quis maximus vitae, faucibus at felis. Proin vel eros orci. Mauris eleifend orci at orci rutrum aliquam. In feugiat vestibulum arcu, quis convallis elit tincidunt eu. Praesent rhoncus varius sem, egestas luctus libero feugiat ac. Nam eget dui et eros tincidunt bibendum. Nullam dignissim, elit ac gravida condimentum, libero lacus condimentum nisi, quis cursus nunc dolor sit amet massa.",
+  },
+];
